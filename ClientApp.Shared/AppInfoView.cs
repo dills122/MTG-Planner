325452b19f4b64d6087f9fb6b46a9f8cc866bc64
Dylan Steele <dylansteele57@gmail.com>
2018-11-10 23:12:15 -0500

    Created Clients and Tested Orleans

---
+++ ClientApp.Shared/AppInfoView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp.Shared
+{
+    public class AppInfoView
+    {
+        public string AppName { get; set; }
+        public Guid AppId { get; set; }
+    }
+}
