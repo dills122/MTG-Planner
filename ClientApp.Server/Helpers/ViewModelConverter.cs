a3c17534099963c07648e23d9576176e692b79e3
Dylan Steele <dylansteele57@gmail.com>
2018-11-11 15:46:58 -0500

    Added set vm transforms.

--- ClientApp.Server/Helpers/ViewModelConverter.cs
+++ ClientApp.Server/Helpers/ViewModelConverter.cs
@@ -1,12 +1,13 @@
 ﻿using ClientApp.Shared;
 using MtgApiManager.Lib.Model;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ClientApp.Server.Helpers
 {
+    /// <summary>
+    /// Used for Transforming to Appropriate View Model
+    /// </summary>
     public static class ViewModelConverter
     {
 
@@ -15,10 +16,22 @@ namespace ClientApp.Server.Helpers
         {
             return new CardViewModel
             {
+                Name = card.Name,
+                Text = card.Text,
+                OriginalText = card.OriginalText,
+                Artist = card.Artist,
+                ReleaseDate = card.ReleaseDate,
+                ColorIdentity = card.ColorIdentity.ToList(),
+                Power = card.Power,
+                Toughness = card.Toughness,
+                Rarity = card.Rarity,
+                Set = card.SetName,
+                SetCode = card.Set,
+                Type = card.Type,
                 Colors = card.Colors.ToList(),
                 ManaCost = card.ManaCost,
                 ImgUrl = card.ImageUrl.ToString(),
-                Name = card.Name
+
             };
         }
 
@@ -26,10 +39,43 @@ namespace ClientApp.Server.Helpers
         {
             return cards.Select(c => new CardViewModel
             {
+                Name = c.Name,
+                Text = c.Text,
+                OriginalText = c.OriginalText,
+                Artist = c.Artist,
+                ReleaseDate = c.ReleaseDate,
+                ColorIdentity = c.ColorIdentity.ToList(),
+                Power = c.Power,
+                Toughness = c.Toughness,
+                Rarity = c.Rarity,
+                Set = c.SetName,
+                SetCode = c.Set,
+                Type = c.Type,
                 Colors = c.Colors.ToList(),
                 ManaCost = c.ManaCost,
-                Name = c.Name,
                 ImgUrl = c.ImageUrl.ToString(),
+            }).ToList();
+        }
+
+        public static SetViewModel ToSetViewModel(this Set set)
+        {
+            return new SetViewModel
+            {
+                Name = set.Name,
+                Code = set.Code,
+                Expansion = set.Expansion,
+                ReleaseDate = set.ReleaseDate
+            };
+        }
+
+        public static List<SetViewModel> ToSetViewModel(this List<Set> sets)
+        {
+            return sets.Select(s => new SetViewModel
+            {
+                Name = s.Name,
+                Code = s.Code,
+                Expansion = s.Expansion,
+                ReleaseDate = s.ReleaseDate
             }).ToList();
         }
     }
---
+++ ClientApp.Shared/SetViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientApp.Shared
+{
+    public class SetViewModel
+    {
+        public string Name { get; set; }
+        public string Expansion { get; set; }
+        public string Code { get; set; }
+        public string ReleaseDate { get; set; }
+
+    }
+}
