730c7baa3921dc699207f37fd4b9ff18bbfedf2e
Dylan Steele <dylansteele57@gmail.com>
2019-03-23 12:43:11 -0400

    Added sets to db context

--- Repositories/StateContext.cs
+++ Repositories/StateContext.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BusinessObjects.StateObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repositories
 {
     public class StateContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Deck> Decks { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=StockPal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Initial Catalog=Mtg-Deck-Planner;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
